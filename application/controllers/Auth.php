c09f1229896d9a5dbbe7f38929325489d7a86476
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2021-04-20 12:47:19 +0700

    menambahkan fitur upload

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -106,7 +106,7 @@ class Auth extends CI_Controller
                 'image' => 'default.jpg',
                 'date_created' => time(),
                 'is_active' => 1,
-                'role_id' => 2
+                'role_id' => 3
             ];
 
             $this->db->insert('mahasiswa', $data);
@@ -117,14 +117,66 @@ class Auth extends CI_Controller
 
     public function regisTutor()
     {
-        $data['fakultas'] = $this->db->get('fakultas')->result_array();
+        $this->form_validation->set_rules('nama', 'Nama', 'required|trim');
+        $this->form_validation->set_rules('fakultas', 'Fakultas', 'required|trim');
+        $this->form_validation->set_rules('jurusan', 'Jurusan', 'required|trim');
+        $this->form_validation->set_rules('matkul', 'Matkul', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[tutor.email]|callback_email_check', [
+            'is_unique' => 'Email ini telah digunakan'
+            // 'email_check' => 'Harus menggunakan email IPB University'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password tidak sama',
+            'min_length' => 'Password terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
-        $data['matkul'] = $this->db->get('matkul')->result_array();
+        if ($this->form_validation->run() == false) {
+            $data['fakultas'] = $this->db->get('fakultas')->result_array();
+            $data['matkul'] = $this->db->get('matkul')->result_array();
+            $data['title'] = 'Tutory';
+            $this->load->view('templates/header', $data);
+            $this->load->view('daftar/tutor', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $email = $this->input->post('email', true);
+            $upload_image = $_FILES['nilai']['name'];
+            if ($upload_image) {
+                $config['upload_path'] = './assets/img/nilai';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']     = '1024';
+                $this->load->library('upload', $config);
+                if ($this->upload->do_upload('nilai')) {
+                    $nilai = $this->upload->data('file_name');
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Gambar harus sesuai dengan format</div>');
 
-        $data['title'] = 'Tutory';
-        $this->load->view('templates/header', $data);
-        $this->load->view('daftar/tutor', $data);
-        $this->load->view('templates/footer');
+                    redirect('auth/regisTutor');
+
+                    die;
+                }
+            }
+            // var_dump($upload_image);
+            // die;
+            $data = [
+                'nama' => htmlspecialchars($this->input->post('nama', true)),
+                'nim' => htmlspecialchars($this->input->post('nim', true)),
+                'email' => htmlspecialchars($email),
+                'fakultas' => htmlspecialchars($this->input->post('fakultas', true)),
+                'jurusan' => $this->input->post('jurusan', true),
+                'matkul' => $this->input->post('matkul', true),
+                'nilai' => $nilai,
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'image' => 'default.jpg',
+                'date_created' => time(),
+                'is_active' => 1,
+                'role_id' => 2
+            ];
+
+            $this->db->insert('tutor', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please Login</div>');
+            redirect("Auth");
+        }
     }
 
     public function cekJurusan()
